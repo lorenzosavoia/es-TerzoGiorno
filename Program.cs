168b5db4371d53fd9fcff61a520356673b6f4e83
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-12 14:38:00 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TerzoGiorno // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        /*
+        // creo struttura struct (simile a classe con dei limiti)
+        public struct Persona
+        {
+            public string sNome;
+            public string sCognome;
+            public DateTime DataDiNascita;
+        }
+        static void QualcheMetodo(string sAppo)
+        {
+            ShowMessage smMioMessaggio = new ShowMessage();
+
+            Console.WriteLine("Buongiorno csharp");
+        }
+
+        static void Main(string[] args)
+        {
+            QualcheMetodo("pluto");
+
+            int iMioIntero;
+            iMioIntero = 30;
+
+            //richiamo lo struct
+            Persona pMiaPersona;
+            // richiamo lo struct e lo creo assegnando ad esso dei valori 
+            pMiaPersona = new Persona();
+            pMiaPersona.sNome = "Lorenzo";
+            pMiaPersona.sCognome = "Savoia";
+            pMiaPersona.DataDiNascita = new DateTime(1997, 07, 21);
+        */
+        static void Main(string[] args)
+        {
+
+
+        }
+
+
+    }
+}
+
+
---
+++ showmessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TerzoGiorno
+{
+    class ShowMessage
+    {
+        
+        public ShowMessage()
+        {
+            Console.WriteLine("Hi, i am default");
+        }
+
+        public ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        ~ShowMessage()
+        {
+            Console.WriteLine("I am destructor and I clean the resources and free up memory as soon as program closed");
+            Console.ReadLine();
+        }
+    }
+    class DieciCaratteri
+    {
+        private int iDimensioneFinestra = 10;
+        private int sStringaCorrente;
+    }
+
+}
+
